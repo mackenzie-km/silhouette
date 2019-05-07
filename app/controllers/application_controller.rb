5dd694f89572f44081a96cae6492a0342efdabfc
Mackenzie Moore <macmoore@ucdavis.edu>
2019-05-07 15:39:51 -0700

    Added in basic class inheritance and setup for controllers & models

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+  configure do
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
+  end
+end
