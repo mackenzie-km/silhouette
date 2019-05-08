3d57d50098bd25404ab7f13707371c7913ea5b16
Mackenzie Moore <macmoore@ucdavis.edu>
2019-05-07 17:01:19 -0700

    add in basic controller actions for creating, editing, deleting, viewing contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,12 @@
 class Contact < ActiveRecord::Base
   belongs_to :user
+  
+  def full_name
+    self.first_name + " " + self.last_initial + "."
+  end
+
+  def profile
+    "/#{self.id}"
+  end
+
 end
