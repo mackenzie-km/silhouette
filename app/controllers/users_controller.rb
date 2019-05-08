70a9c17c9afce074eacf00772ceb931eb15f7ffb
Mackenzie Moore <mackenzie.gonzales.k@gmail.com>
2019-05-08 16:13:39 -0700

    minor refactoring changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,7 @@ class UsersController < ApplicationController
     @user = User.find_by(username: params[:username])
     if !!@user && !!@user.authenticate(params[:password])
       session[:user_id] = @user.id
+      binding.pry
       if @user.contacts.count == 0
         erb :'/contacts/new'
       else
