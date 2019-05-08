104b90c29d2c85a4bc60fbf77d5df1af46b4a914
Mackenzie Moore <mackenzie.gonzales.k@gmail.com>
2019-05-07 17:28:23 -0700

    added user controller functionality. need to fix session bug, add in logged_in? helper method and functionality, and troubleshoot id= errors for both user and contacts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
-class UsersController < ApplicationController 
+class UsersController < ApplicationController
+  get "/users/new" do
+    erb :'/users/new'
+  end
+
+  post "/users" do
+    @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
+    redirect to "/contacts/new"
+  end
+
+  get "/users/login" do
+    erb :'/users/login'
+  end
+
+  post "/users/login" do
+    @user = User.find(params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to "/contacts"
+    else
+      redirect to "/users/login"
+    end
+  end
 end
