75bf4895194e3d92c04c1f7495bde79517cc0bb8
Mackenzie Moore <mackenzie.gonzales.k@gmail.com>
2019-05-07 17:01:19 -0700

    add in basic controller actions for creating, editing, deleting, viewing contact

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ class ApplicationController < Sinatra::Base
     enable :sessions
     set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
   end
+
+  get "/" do
+    erb :index
+  end
 end
--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,40 @@
-class ContactsController < ApplicationController 
+class ContactsController < ApplicationController
+  get "/contacts" do
+    @contacts = Contact.all
+    erb :'/contacts/index'
+  end
+
+  get "/contacts/new" do
+    erb :'/contacts/new'
+  end
+
+  post "/contacts" do
+    @contact = Contact.create(:first_name => params[:first_name], :last_initial => params[:last_initial], :user_id => session[:user_id])
+    redirect to "/contacts/#{@contact.id}"
+  end
+
+  get "/contacts/:id" do
+    @contacts = Contact.find(params[:id])
+    erb :'/contacts/show'
+  end
+
+  get "/contacts/:id/edit" do
+    @contact = Contact.find(params[:id])
+    erb :'/contacts/edit'
+  end
+
+  patch "/contact/:id" do
+      @contact = Contact.find(params[:id])
+      @contact.first_name = params[:first_name]
+      @contact.last_initial = params[:last_initial]
+      @contact.save
+      redirect to "/contacts/#{@contact.id}"
+  end
+
+  delete "/articles/:id" do
+    @contact = Contact.find(params[:id])
+    @contact.delete
+    redirect to "/contacts" 
+  end
+
 end
