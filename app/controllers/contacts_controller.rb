3cfbfdf541bf657d6c823deab4cfc5020f7f8acd
Mackenzie Moore <macmoore@ucdavis.edu>
2019-05-10 16:27:39 -0700

    Fixed patch bugs so contact can be edited.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,7 +2,7 @@ class ContactsController < ApplicationController
 
   get "/contacts" do
     if logged_in?
-      @contacts = Contact.all
+      @contacts = Contact.order(first_name: :asc)
       erb :'/contacts/index'
     else
       redirect to "/users/login"
@@ -18,16 +18,12 @@ class ContactsController < ApplicationController
   end
 
   post "/contacts" do
-    @contact = Contact.new(:first_name => params[:first_name].capitalize, :last_initial => params[:last_initial].capitalize, :user_id => session[:user_id])
+    @contact = Contact.new(:first_name => params[:first_name].capitalize, :last_initial => params[:last_initial].capitalize, :user_id => session[:user_id], :photo => params[:photo])
     if @contact.save
       details = Fact.normalize(params[:facts])
       details.each do |fact|
         fact_object = Fact.new(:topic => fact[0], :information => fact[1], :contact_id => @contact.id)
-        if fact_object.save
-          @contact.facts << fact_object
-        else
-          flash[:message] = "Invalid format. Please try again."
-        end
+        @contact.facts << fact_object
       end
       redirect to "/contacts/#{@contact.id}"
     else
@@ -37,7 +33,7 @@ class ContactsController < ApplicationController
 
   get "/contacts/:id" do
     @contact = Contact.find(params[:id])
-    if logged_in? && (session[:user_id] == @contact.user_id)
+    if right_user?(@contact)
       erb :'/contacts/show'
     else
       redirect to "/users/login"
@@ -46,7 +42,7 @@ class ContactsController < ApplicationController
 
   get "/contacts/:id/edit" do
     @contact = Contact.find(params[:id])
-    if logged_in? && (session[:user_id] == @contact.user_id)
+    if right_user?(@contact)
       erb :'/contacts/edit'
     else
       redirect to "/users/login"
@@ -55,19 +51,31 @@ class ContactsController < ApplicationController
 
   patch "/contacts/:id" do
       @contact = Contact.find(params[:id])
-      if logged_in? && (session[:user_id] == @contact.user_id)
-        @contact.first_name = params[:first_name]
-        @contact.last_initial = params[:last_initial]
-        @contact.save
+      if right_user?(@contact)
+        if params[:first_name] != "" then @contact.first_name = params[:first_name] end
+        if params[:last_initial] != "" then @contact.last_initial = params[:last_initial] end
+
+        details = Fact.normalize(params[:facts])
+        details.each do |fact|
+          fact = Fact.find_by(:topic => fact[0], :contact_id => @contact.id)
+            if fact != nil
+              Fact.update(fact.id, :information => fact[1])
+              fact.save
+            else
+              fact_object = Fact.new(:topic => fact[0], :information => fact[1], :contact_id => @contact.id)
+              fact_object.save
+              @contact.facts << fact_object
+            end
+        end
         redirect to "/contacts/#{@contact.id}"
       else
         redirect to "/users/login"
       end
   end
 
-  delete "/articles/:id" do
+  delete "/contacts/:id" do
     @contact = Contact.find(params[:id])
-    if logged_in? && (session[:user_id] == @contact.user_id)
+    if right_user?
       @contact.delete
       redirect to "/contacts"
     else
